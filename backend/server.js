dee8c0e7de67af3db83387bd0d2369ab8645d392
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2025-06-30 00:36:26 +0100

    linting was working fine

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import Fastify from "fastify";
 
 const fastify = Fastify({
-    logger: true
-})
+  logger: true,
+});
 
 const start = async () => {
-    try {
-        await fastify.listen({ port: 3000 })
-    } catch (err) {
-        fastify.log(err)
-        process.exit(1)
-    }
-}
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log(err);
+    process.exit(1);
+  }
+};
 
-start()
+start();
