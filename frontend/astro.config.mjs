258abc00a05372553d67fa3f8c9211cd6e8aba59
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2025-07-02 17:04:12 +0100

    pnpm workflow

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
 });
